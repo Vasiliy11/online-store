f2d95cec86536eb8959009c9c8808d78e345dfa0
Vasiliy11 <vasiliy13410@gmail.com>
2020-09-26 18:51:20 +0300

    Made responsive banner section

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,15 @@ $(function(){
     $('.banner-section__slider').slick({
         dots:true,
         prevArrow: '<button class="slick-arrows slick-arrows--prev"><img src="./images/arrow-left.svg" alt=""></button>' ,   
-        nextArrow: '<button class="slick-arrows slick-arrows--next"><img src="./images/arrow-right.svg" alt=""></button>'    
+        nextArrow: '<button class="slick-arrows slick-arrows--next"><img src="./images/arrow-right.svg" alt=""></button>' ,
+        responsive: [
+            {
+                breakpoint: 969,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]   
     });
 
     $('.tab').on('click', function(e){
